409951151f7818bac0f1174892a51aebc0c83d43
Alexander Staeding <alexanderstaeding@gmail.com>
2021-11-12 17:02:09 +0100

    Formatting

--- grader/src/main/java/h02/H02_RubricProvider.java
+++ grader/src/main/java/h02/H02_RubricProvider.java
@@ -1,6 +1,5 @@
 package h02;
 
-
 import org.sourcegrade.jagr.api.rubric.*;
 import org.sourcegrade.jagr.api.testing.ClassTransformer;
 import org.sourcegrade.jagr.api.testing.RubricConfiguration;
@@ -109,7 +108,7 @@ public class H02_RubricProvider implements RubricProvider {
     )
     .build();
 
-  public static final Criterion H3_2_T2 = Criterion.builder()      
+  public static final Criterion H3_2_T2 = Criterion.builder()
     .shortDescription("H3_2_T2: orderThreeInts ordnet in einem Array aus drei distinkten Integer diese aufsteigend nach der Größe und gibt das sortierte Array zurück.")
     .grader(
       Grader.testAwareBuilder()
